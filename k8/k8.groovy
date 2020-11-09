deec1a3bb38a3d79a3630d9eb44ac4a79ebfd7fe
root <root@ip-172-31-43-115.ap-south-1.compute.internal>
2020-11-09 11:26:31 +0000

    all

--- k8/k8.groovy
+++ k8/k8.groovy
@@ -1,13 +1,46 @@
 node {
 	def rootdir="/home/ubuntu"
+	def hostname="master-node"
 
 	stage ('Loading pipelines scripts '){
 		checkout scm 
 		common=load "${rootdir}/exp/Common.groovy"
 }
 
-	stage ('update the syste'){
+	stage ('update the system'){
 		common.upgrade()
+}
+	stage ('Install Docker'){
+		common.docker()
+}
+	stage ('enable docker'){
+		common.docker-enable()
+}
+	stage ('start docker'){
+		common.docker-start()
+}
+
+	stage ('installing curl and wget'){
+		common.curl-wget()
+}
+	stage ('preparing prerequsites for kub adding key'){
+                common.addkey()
+}
+
+	stage ('add kub repo'){
+		common.addrepo()
+}
+	stage ('installing kub tools and keeping in hold'){
+		common.kubctl()
+}
+	stage ('setting up hostname for kub master'){
+		common.hostname(hostname)
+}
+	stage ('opening n/w for other nodes'){
+		common.addnode()
+}
+	stage ('finalizing kubernetes'){
+		common.kubfinal()
 }
 }
 
