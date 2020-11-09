7c9bb98e252a22e265f7b9201633a606555a5a75
root <root@ip-172-31-43-115.ap-south-1.compute.internal>
2020-11-09 09:19:36 +0000

    first

---
+++ elk/Common.groovy
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;		
+import java.util.regex.Pattern;		
+import groovy.json.JsonSlurper;
+import groovy.json.JsonSlurperClassic
+import groovy.json.*
+
+import java.util.Map;
+import java.io.Reader;
+import java.util.HashMap;
+
+
+def createfile (def filename, def path){
+	sh """cd $path """
+	sh """ touch $filename """
+}
+
+
+def addrepo(def filename, def path){
+	sh """ echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee $path/$filename"""
+}
+
+def 
+return this 
