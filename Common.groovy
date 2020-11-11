a2d2e022735ff604e111757f05776ce8f69d2cc2
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2020-11-11 14:02:37 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -76,10 +76,10 @@ def loginaws(){
 	sh """ sudo aws sts get-caller-identity """
 }
 def createInstance(def Instanceid){
+	sh """ > /tmp/any.txt """
 	sh """ sudo aws ec2 run-instances --image-id ami-0a4a70bd98c6d6441 --count 1 --instance-type t2.medium --key-name atos --security-group-ids sg-cf6543ab --subnet-id subnet-4a020d22  > /tmp/instance.id """
 	sh """ sleep 15"""
 	sh '''  
-		> /tmp/any.txt
 		Instanceid=$(cat /tmp/any.txt | awk '{print $9}')
 		sudo aws ec2 create-tags --resources $Instanceid --tags Key=Name,Value=Petclinic 
 	   '''
