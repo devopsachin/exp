f5602d3f4a369db23aa0f63b9274723089f60536
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2020-11-11 14:08:23 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -77,7 +77,7 @@ def loginaws(){
 }
 def createInstance(def Instanceid){
 	sh """ sudo rm -rf /tmp/any.txt && sudo touch /tmp/any.txt"""
-	sh """ sudo aws ec2 run-instances --image-id ami-0a4a70bd98c6d6441 --count 1 --instance-type t2.medium --key-name atos --security-group-ids sg-cf6543ab --subnet-id subnet-4a020d22  > /tmp/any.txt """
+	sh ''' sudo aws ec2 run-instances --image-id ami-0a4a70bd98c6d6441 --count 1 --instance-type t2.medium --key-name atos --security-group-ids sg-cf6543ab --subnet-id subnet-4a020d22  >> /tmp/any.txt '''
 	sh """ sleep 15"""
 	sh '''  
 		Instanceid=$(cat /tmp/any.txt | awk '{print $9}')
