d206c3fd3ce9bce5f797bd55c53e7ffd18a87992
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2020-11-11 16:56:46 +0530

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -88,7 +88,8 @@ def ssh(){
 		sleep 15
 		Instanceid=$(cat /tmp/instance.id | awk 'FNR==2 {print $9}')
 		Ip=$(sudo aws ec2 describe-instances --instance-ids=$Instanceid  --query 'Reservations[*].Instances[*].{Instance:PublicIpAddress}')
-		sudo ssh -o "StrictHostKeyChecking no" -i "/tmp/atos.pem" ubuntu@$Ip && sudo hostname -i && sudo hostname 
+		sudo ssh -o "StrictHostKeyChecking no" -i "/tmp/atos.pem" ubuntu@$Ip 
+		sudo scp -i "/tmp/atos.pem" remotescript.sh ubuntu@$Ip:/tmp
 		 
 		
 	   '''
